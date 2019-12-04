2630976359a464d2e837335fc718915fb2dc2dcc
raschutz <43079674+raschutz@users.noreply.github.com>
2019-12-04 22:11:02 +0100

    Update Database.js

--- js/Database.js
+++ js/Database.js
@@ -16,5 +16,48 @@
  */
 
 class Database {
+   const MongoClient = require('mongodb').MongoClient;
+
+   var url = "mongodb+srv://dbUser:$nak3!@cluster0-rmmze.mongodb.net/test?retryWrites=true&w=majority";
+   
+   // finds high score in database
+   findHighScore () {
+         MongoClient.connect(url, function(err, db) {
+            if (err) throw err;
+            var dbo = db.db("snake");
+            var mysort = { player_score: -1 };
+            dbo.collection("scores").find({}, { projection: { _id: 0, player_intl: 1, player_score: 1 } }).sort(mysort).toArray(function(err, result) {
+                if (err) throw err;
+                db.close();
+                return result[0].player_score;
+            });
+         });
+   }
+
+   //finds initials associated with highscore in database
+   findInitials () {
+        MongoClient.connect(url, function(err, db) {
+             if (err) throw err;
+             var dbo = db.db("snake");
+             var query = { player_score: + highScore };
+             dbo.collection("scores").find(query).sort(mysort).toArray(function(err, result) {
+                 if (err) throw err;
+                 db.close();
+                 return result[0].player_intl;
+             });
+        });
+   }
+
+  // inserts user provided initials and score from current game session into database
+  insertInitialsAndScore () {
+
+
+  }
+  
+  // finds top 10 scores from database and puts into array for display
+  topScores() {
+
+ }
+
 
 }
